2e7ce006ec4be8aa3d3663c13c1ef7adff5caecd
Pedro Marins <phmarins@hotmail.com>
2020-06-28 23:22:59 -0300

    refactor: move SUT creating to a factory method

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,15 +2,32 @@ import { RemoteAuthentication } from './remote-authentication';
 
 import { HttpPostClientSpy } from '../../test/mock-http-client';
 
+type SutTypes = {
+    systemUnderTest: RemoteAuthentication;
+    httpPostClientSpy: HttpPostClientSpy;
+};
+
+const makeSystemUnderTest = (url: string = 'any_url'): SutTypes => {
+    const httpPostClientSpy: HttpPostClientSpy = new HttpPostClientSpy();
+    const systemUnderTest: RemoteAuthentication = new RemoteAuthentication(
+        url,
+        httpPostClientSpy
+    );
+
+    return {
+        systemUnderTest,
+        httpPostClientSpy
+    };
+};
+
 describe('RemoveAuthentication', () => {
-    test('Should call HttpPostClient with correct URL', async() => {
-        const url: string = 'any_url';
-        const httpPostClient: HttpPostClientSpy = new HttpPostClientSpy();
-        const systemUnderTest: RemoteAuthentication = new RemoteAuthentication(
-            url,
-            httpPostClient
-        );
+    test('Should call HttpPostClient with correct URL', async(): Promise<void> => {
+        const url: string = 'other_url';
+        const {
+            systemUnderTest,
+            httpPostClientSpy
+        } = makeSystemUnderTest(url);
         await systemUnderTest.auth();
-        expect(httpPostClient.url).toBe(url);
+        expect(httpPostClientSpy.url).toBe(url);
     });
 });
