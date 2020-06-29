890e432e8a76e55c872e1bdad77498b257d407eb
Pedro Marins <phmarins@hotmail.com>
2020-06-28 23:59:48 -0300

    refactor: change HttpPostClient to receive an object

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,3 +1,7 @@
+export type HttpPostParams = {
+    url: string;
+};
+
 export interface HttpClientPost {
-    post(url: string): Promise<void>;
+    post(params: HttpPostParams): Promise<void>;
 }
--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,9 +1,12 @@
-import { HttpClientPost } from '../protocols/http/http-post-client';
+import {
+    HttpClientPost,
+    HttpPostParams
+} from '../protocols/http/http-post-client';
 
 export class HttpPostClientSpy implements HttpClientPost {
     url?: string;
-    async post(url: string): Promise<void> {
-        this.url = url;
+    async post(params: HttpPostParams): Promise<void> {
+        this.url = params.url;
         return await Promise.resolve();
     }
 }
