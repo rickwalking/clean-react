6343e63e5ef56f4f616ac8c9d4141db24a06c473
Pedro Marins <phmarins@hotmail.com>
2020-07-02 01:53:35 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,9 +1,13 @@
-import { HttpPostParams } from '@/data/protocols/http';
+import { HttpPostParams, HttpResponse, HttpClientPost } from '@/data/protocols/http';
 
 import axios from 'axios';
 
-export class AxiosHttpClient {
-    async post(params: HttpPostParams<any>): Promise<any> {
-        await axios.post(params.url, params.body);
+export class AxiosHttpClient implements HttpClientPost<any, any> {
+    async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+        const httpResponse = await axios.post(params.url, params.body);
+        return {
+            statusCode: httpResponse.status,
+            body: httpResponse.data
+        };
     }
 }
