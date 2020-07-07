0c3929d6e830a3d959ab8a075fa4d2d88168de5a
Pedro Marins <phmarins@hotmail.com>
2020-07-07 00:48:22 -0300

    refactor: add index files with multiple imports

---
+++ src/presentation/components/index.ts
@@ -0,0 +1,18 @@
+export {
+    default as Footer
+} from '@/presentation/components/footer/footer';
+export {
+    default as FormStatus
+} from '@/presentation/components/form-status/form-status';
+export {
+    default as Input
+} from '@/presentation/components/input/input';
+export {
+    default as Spinner
+} from '@/presentation/components/spinner/spinner';
+export {
+    default as LoginHeader
+} from '@/presentation/components/login-header/login-header';
+export {
+    default as Logo
+} from '@/presentation/components/logo/logo';
