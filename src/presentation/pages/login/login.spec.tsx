dec7dec2d51605e3453f5a215dc40e70f213d7e3
Pedro Marins <phmarins@hotmail.com>
2020-07-14 21:59:24 -0300

    test: ensure Login present spinner on submit

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -96,4 +96,16 @@ describe('Login component', () => {
         const submitButton = sut.getByTestId('submit') as HTMLButtonElement;
         expect(submitButton.disabled).toBe(false);
     });
+
+    test('Should show spinner on submit', (): void => {
+        const { sut } = makeSut();
+        const emailInput = sut.getByTestId('email');
+        fireEvent.input(emailInput, { target: { value: faker.internet.email() } });
+        const passwordInput = sut.getByTestId('password');
+        fireEvent.input(passwordInput, { target: { value: faker.internet.password() } });
+        const submitButton = sut.getByTestId('submit') as HTMLButtonElement;
+        fireEvent.click(submitButton);
+        const spinner = sut.getByTestId('spinner');
+        expect(spinner).toBeTruthy();
+    });
 });
