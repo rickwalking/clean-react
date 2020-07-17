45e8bf8cb1c6e25f222cbdfe234962531b1e80d9
Pedro Marins <phmarins@hotmail.com>
2020-07-17 00:34:56 -0300

    test: move SUIT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -161,4 +161,12 @@ describe('Login component', (): void => {
         simulateValidSubmit(sut);
         expect(authenticationSpy.callsCount).toBe(1);
     });
+
+    test('Should not call Authentication if form is invalid', (): void => {
+        const validationError = faker.random.words();
+        const { sut, authenticationSpy } = makeSut({ validationError });
+        populateEmailField(sut);
+        fireEvent.submit(sut.getByTestId('form'));
+        expect(authenticationSpy.callsCount).toBe(0);
+    });
 });
