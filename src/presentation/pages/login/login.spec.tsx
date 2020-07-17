78878b7407f385a38623a39194ca06168b4a7908
Pedro Marins <phmarins@hotmail.com>
2020-07-17 01:02:44 -0300

    test: ensure Lgoin present error and hide spinner on Authentication

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -4,7 +4,8 @@ import {
     render,
     RenderResult,
     fireEvent,
-    cleanup
+    cleanup,
+    waitFor
 } from '@testing-library/react';
 
 import Login from '@/presentation/pages/login/login';
@@ -15,6 +16,7 @@ import {
 } from '@/presentation/test';
 
 import faker from 'faker';
+import { InvalidCredentialsError } from '@/domain/errors';
 
 type SutTypes = {
     sut: RenderResult;
@@ -168,5 +170,19 @@ describe('Login component', (): void => {
         populateEmailField(sut);
         fireEvent.submit(sut.getByTestId('form'));
         expect(authenticationSpy.callsCount).toBe(0);
+    });
+
+    test('Should present error if Authentication fails', async(): Promise<void> => {
+        const { sut, authenticationSpy } = makeSut();
+        const error = new InvalidCredentialsError();
+        jest.spyOn(authenticationSpy, 'auth').mockReturnValueOnce(
+            Promise.reject(error)
+        );
+        simulateValidSubmit(sut);
+        const errorWrap = sut.getByTestId('error-wrap');
+        await waitFor(() => errorWrap);
+        const mainError = sut.getByTestId('main-error');
+        expect(mainError.textContent).toBe(error.message);
+        expect(errorWrap.childElementCount).toBe(1);
     });
 });
