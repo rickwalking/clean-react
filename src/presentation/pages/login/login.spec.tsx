a12257ad0545715a2abba3074517119b33f31ca6
Pedro Marins <phmarins@hotmail.com>
2020-07-19 21:14:53 -0300

    test: ensure Login neavigates do main page on success!

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -31,7 +31,7 @@ type SutParams = {
     validationError: string;
 };
 
-const history = createMemoryHistory();
+const history = createMemoryHistory({ initialEntries: ['/login'] });
 const makeSut = (params?: SutParams): SutTypes => {
     const validationStub = new ValidationStub();
     const authenticationSpy = new AuthenticationSpy();
@@ -204,6 +204,8 @@ describe('Login component', (): void => {
             'accessToken',
             authenticationSpy.account.accessToken
         );
+        expect(history.length).toBe(1);
+        expect(history.location.pathname).toBe('/');
     });
 
     test('Should go to sign up page', (): void => {
