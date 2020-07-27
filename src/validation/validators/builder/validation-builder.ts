bf8636af3b9c01269793a0081e1c2d5ecca5ac58
Pedro Marins <phmarins@hotmail.com>
2020-07-26 23:17:11 -0300

    feat: ensure ValidationBuilder has an email helper

--- src/validation/validators/builder/validation-builder.ts
+++ src/validation/validators/builder/validation-builder.ts
@@ -1,5 +1,6 @@
 import {
-    RequiredFieldValidation
+    RequiredFieldValidation,
+    EmailValidation
 } from '@/validation/validators';
 
 import { FieldValidation } from '@/validation/protocols/field-validation';
@@ -16,6 +17,11 @@ export class ValidationBuilder {
 
     required(): ValidationBuilder {
         this.validations.push(new RequiredFieldValidation(this.fieldName));
+        return this;
+    }
+
+    email(): ValidationBuilder {
+        this.validations.push(new EmailValidation(this.fieldName));
         return this;
     }
 
